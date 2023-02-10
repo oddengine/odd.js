70352a8c6a7e55d997a6b6e3e961449b0329d4d3
spencer <670292548@qq.com>
2023-02-10 20:18:24 +0800

    v2.4.30 - Send recorded stream to server.

--- src/utils/utils.streamsaver.js
+++ src/utils/utils.streamsaver.js
@@ -30,7 +30,7 @@
             _writer.closed.then(function () {
                 _logger.log('StreamWriter.closed: ' + _this.filename);
                 _this.close();
-            });
+            }).catch(function (err) { });
         }
 
         // Should be called once the first keyframe is detected.
